c86d0a51c97b7556b00a393dd10a0405f83c52b6
Brijeshgrg1999 <118506279+Brijeshgrg1999@users.noreply.github.com>
2023-01-07 11:06:53 +0800

    Add files via upload

---
+++ doubleLList.cpp
@@ -0,0 +1,64 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std; 
+
+// create node  to store data and link 
+
+struct Node {
+    int data ; 
+    struct Node *next; 
+    struct Node* prev ; 
+};
+
+struct Node* head ;  // global variable - pointer to head 
+
+
+struct Node* getNewNode(int x ){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = x;
+    temp->prev= NULL ; 
+    temp->next = NULL ; 
+    return temp ; 
+
+
+}
+
+void InsertHead(int x) 
+{
+    struct Node* newNode = getNewNode(x);
+    if(head == NULL){
+        head = newNode; //newNode is the pointer variable which is storing the addres of head now ; 
+        return ; 
+    }
+
+    head->prev = newNode ;
+    newNode->next = head; 
+    head = newNode; 
+
+}
+
+void print() 
+{
+    struct Node* temp = head ;
+    printf("forward:") ;
+    while (temp!= NULL) 
+    {
+        printf("%d" , temp->data);
+        temp = temp->next ; 
+    }
+    printf("\n") ; 
+}
+
+int main(){
+
+InsertHead(2);
+InsertHead(5);
+InsertHead(2);
+print();
+
+return 0 ; 
+
+};
